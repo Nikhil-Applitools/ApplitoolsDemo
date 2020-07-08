c1a6afd14e0cfab7614860c6a16e447c2a288c1e
satishapplitools <50477069+satishapplitools@users.noreply.github.com>
2020-07-08 14:39:38 +1000

    Update DemoApp.java

--- src/test/java/DemoApp/DemoApp.java
+++ src/test/java/DemoApp/DemoApp.java
@@ -90,6 +90,7 @@ public class DemoApp {
         options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
+         options.addArguments("--headless");
 
 
         // Use Chrome browser
